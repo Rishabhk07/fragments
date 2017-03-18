3e8df78726bfe11c1045c8d7766a51c740d73fb1
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-03-18 11:23:48 +0530

    initial commit
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/com/condingblocks/fragments/FramentBlue.java
@@ -0,0 +1,36 @@
+package com.condingblocks.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by rishabhkhanna on 18/03/17.
+ */
+
+public class FramentBlue extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragmentblue , container , false);
+        Button btn = (Button) rootView.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+}
